5f24995aa8647b2c4c3b7cee3d1de9ce2114d5e5
Andrew - MacMini <andrewmiotke@gmail.com>
2018-04-28 10:46:41 -0700

    Created more classes

--- ObjectsAndClasses.playground/Contents.swift
+++ ObjectsAndClasses.playground/Contents.swift
@@ -55,6 +55,21 @@ newVW.motor = "1.8L 16v"
 print("My new Volkswagen is a \(newVW.generation) \(newVW.model) \(newVW.subModel), with a \(newVW.motor)")
 
 
+// Audi class
+class Audi {
+    var generation = ""
+    var model = ""
+    var type = ""
+}
+
+let wantedAudi = Audi()
+wantedAudi.generation = "B5"
+wantedAudi.model = "RS 4"
+wantedAudi.type = "Avant"
+
+print("Audi, we'll settle for new \(wantedAudi.model) \(wantedAudi.type)'s." )
+print("However, the \(wantedAudi.generation) \(wantedAudi.model) is what I really want.")
+
 
 
 
