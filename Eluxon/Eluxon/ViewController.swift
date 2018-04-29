75369dd78927ff740697832eda435fb133750040
Andrew - MacMini <andrewmiotke@gmail.com>
2018-04-29 14:40:45 -0700

    Finished Protocols

--- Eluxon/Eluxon/ViewController.swift
+++ Eluxon/Eluxon/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onOffLbl: UILabel!
+    @IBOutlet weak var toggleBtn: UIButton!
+    
+    var switchStatus: SwitchStatus = .off
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func toggleBtnWasPressed(_ sender: Any) {
+        switchStatus.toggle()
+        if switchStatus == .off {
+            toggleBtn.setImage(UIImage(named: "offBtn")!, for: .normal)
+            view.backgroundColor = #colorLiteral(red: 0.4078193307, green: 0.4078193307, blue: 0.4078193307, alpha: 1)
+            onOffLbl.text = "Off 🌚"
+            onOffLbl.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        } else {
+            toggleBtn.setImage(UIImage(named: "onBtn")!, for: .normal)
+            view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            onOffLbl.text = "On 🌝"
+            onOffLbl.textColor = #colorLiteral(red: 0.4078193307, green: 0.4078193307, blue: 0.4078193307, alpha: 1)
+        }
     }
-
-
 }
 
