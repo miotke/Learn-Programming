b55c30607beeedfb8b5ab7a3fde0691b1c045029
Andrew - MacMini <andrewmiotke@gmail.com>
2018-04-29 09:45:02 -0700

    Continuing with protocols

--- protocols:delegates - numbers.playground/Contents.swift
+++ protocols:delegates - numbers.playground/Contents.swift
@@ -51,3 +51,98 @@ func *(valueA: Number, valueB: Number) -> Float {
 let x: Double = 1.2347
 let y: Int = 5
 let q = x / y
+
+
+class Question {
+    var type: QuestionType
+    var query: String
+    var answer: String
+    
+    init(type: QuestionType, query: String, answer: String) {
+        self.type = type
+        self.query = query
+        self.answer = answer
+    }
+}
+
+enum QuestionType: String {
+    case trueFalse = "The sky is blue."
+    case multipleChoice = "Who is the ugliest Beattle: John, Paul, George, or Ringo?"
+    case shortAnswer = "What is the capital of Oregon?"
+    case essay = "In 50 words, explain molecular fusion."
+    
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay]
+}
+
+enum answerType: String {
+    case trueFalse = "True."
+    case multipleChoice = "Sgt. Pepper."
+    case shortAnswer = "Salem"
+    case essay = "Molecular fusion happens when a daddy molecule and mommy molecule love each other very much."
+    
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay]
+}
+
+
+protocol QuestionGenerator {
+    func generateRandomQuestion() -> Question
+}
+
+class Answer{}
+
+protocol AnswerGenerator: QuestionGenerator {
+    func generateRandomAnswer() -> answerType
+}
+
+class Jeopardy: AnswerGenerator {
+    func generateRandomAnswer() -> answerType {
+        <#code#>
+    }
+    
+    func generateRandomQuestion() -> Question {
+        <#code#>
+    }
+}
+
+class Quiz: QuestionGenerator {
+    func generateRandomQuestion() -> Question {
+        let randomNumeral = Int(arc4random_uniform(4))
+        let randomType = QuestionType.types[randomNumeral]
+        let randomQuery = randomType.rawValue
+        let randomAnswer = answerType.types[randomNumeral].rawValue
+        let randomQuestion = Question(type: randomType, query: randomQuery, answer: randomAnswer)
+        
+        return randomQuestion
+    }
+}
+
+let quiz = Quiz()
+
+let question = quiz.generateRandomQuestion()
+print("Question Type: \(question.type), \n Query: \(question.query), \n Answer: \(question.answer)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
