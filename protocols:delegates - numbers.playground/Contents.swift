f897dc4914ee211d4e4e0f97ad91a22344db0382
Andrew - MacMini <andrewmiotke@gmail.com>
2018-04-29 09:26:11 -0700

    Learning protocols

---
+++ protocols:delegates - numbers.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+protocol Number {
+    var floatValue: Float { get }
+}
+
+
+extension Float: Number {
+    var floatValue: Float {
+        return self
+    }
+}
+
+extension Double: Number {
+    var floatValue: Float {
+        return Float(self)
+    }
+}
+
+extension Int: Number {
+    var floatValue: Float {
+        return Float(self)
+    }
+}
+
+
+extension UInt: Number {
+    var floatValue: Float {
+        return Float(self)
+    }
+}
+
+func +(valueA: Number, valueB: Number) -> Float {
+    return valueA.floatValue + valueB.floatValue
+}
+
+func -(valueA: Number, valueB: Number) -> Float {
+    return valueA.floatValue - valueB.floatValue
+}
+
+func /(valueA: Number, valueB: Number) -> Float {
+    return valueA.floatValue / valueB.floatValue
+}
+
+func *(valueA: Number, valueB: Number) -> Float {
+    return valueA.floatValue * valueB.floatValue
+}
+
+let x: Double = 1.2347
+let y: Int = 5
+let q = x / y
