26a16568ba11a59071670738581f987e0a5a6b6e
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-06 12:00:56 -0700

    Product view is created with IBOutlets

---
+++ CoderSwag/CoderSwag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Andrew on 5/6/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
