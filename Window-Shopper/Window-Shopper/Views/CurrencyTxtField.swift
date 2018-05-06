2a6030ebd7a067c60fc186735cd72634636e3543
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-05 18:04:56 -0700

    Created a custom text field with NSAttributedString and custom place holder text

---
+++ Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyTxtField.swift
+//  Window-Shopper
+//
+//  Created by Andrew on 5/5/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
