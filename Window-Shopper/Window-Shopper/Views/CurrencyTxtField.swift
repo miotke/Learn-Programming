cddf8e26cc8d7c63037f8fdde5af69ac1a92d283
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-05 18:13:56 -0700

    Duplicated the text field, updated constraints, and set keyboard

--- Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
+++ Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
-
+        
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
