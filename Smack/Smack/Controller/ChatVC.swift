a6f420c53e89517c79cabc8e533046f8fd6c476e
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-06 16:58:50 -0700

    Rebuilt the into to Smack

---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Andrew on 5/6/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
