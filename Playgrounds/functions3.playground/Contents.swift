ef383589ac2133e3558887024ecca7392096357e
Andrew MacBookPro(work) <andrewmiotke@gmail.com>
2018-05-07 18:09:08 -0700

    Added another playground

---
+++ Playgrounds/functions3.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var playgroundsString = "Swift Playgrounds are great"
+var sarcasticResponse = "for crashing and not working properly"
+
+func sarcasticFunc(string1: String, string2: String) -> String {
+    print("\(playgroundsString)...\(sarcasticResponse)")
+   return playground
+}
+
+let playground = sarcasticFunc(string1: playgroundsString, string2: sarcasticResponse)
+
+
+func getNumber(numberOne: Double, numberTwo: Double) -> Double {
+    if numberOne * numberTwo == 100 {
+        print("This is correct")
+    } else {
+        print("This is wrong")
+    }
+    return getTheNumber
+}
+
+let getTheNumber = getNumber(numberOne: 49.5, numberTwo: 50.5)
