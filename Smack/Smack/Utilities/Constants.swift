7403da46ee5f0cebe868cbf72b5e5b66e1c81578
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-09 20:44:10 -0700

    Constraints are done

---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Andrew on 5/9/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
