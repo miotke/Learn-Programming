350f71e7460e1aed4be3298dae619a7d65a5feff
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-11 18:55:59 -0700

    Registering accounts now works

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != " " else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != " " else { return  }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+        
+    }
+    
 
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
