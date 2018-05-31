b6df06b17a725b822b7185d3dfd09e9205a448c7
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-30 21:05:14 -0700

    Setup the GoalVC, created the correct constraints and elements, created the IBOutletsand IBActions

--- Goal Post/Goal Post/Controller/GoalsVC.swift
+++ Goal Post/Goal Post/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  Goal Post
 //
 //  Created by Andrew on 5/30/18.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GoalsVC: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+       print("button was pressed")
     }
-
-
 }
 
