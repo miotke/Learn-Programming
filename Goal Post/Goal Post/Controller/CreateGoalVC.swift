c1985615153130ec3cdc4c88a84b4be901a0f491
Andrew - MacMini <andrewmiotke@gmail.com>
2018-05-31 20:01:00 -0700

    Created the CreateGoalVC with elements and setup the IBOutlets and IBActions

---
+++ Goal Post/Goal Post/Controller/CreateGoalVC.swift
@@ -0,0 +1,40 @@
+//
+//  CreateGoalVC.swift
+//  Goal Post
+//
+//  Created by Andrew on 5/31/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermButton: UIButton!
+    @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Actions
+    @IBAction func nextButtonWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermButtonWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermButtonWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backButtonWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
