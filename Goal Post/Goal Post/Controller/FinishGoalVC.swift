7b885945a843793e3064e8db11b1789281fa8889
Andrew - MacMini <andrewmiotke@gmail.com>
2018-06-02 15:26:28 -0700

    Setup Core Data and it saves successfully.

--- Goal Post/Goal Post/Controller/FinishGoalVC.swift
+++ Goal Post/Goal Post/Controller/FinishGoalVC.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
-
+    
     //Outlets
     @IBOutlet weak var createGoalButton: UIButton!
     @IBOutlet weak var pointsTextField: UITextField!
@@ -19,20 +20,52 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         createGoalButton.bindToKeyboard()
         pointsTextField.delegate = self
     }
     
     func initData(description: String, type: GoalType) {
-       self.goalDescription = description
+        self.goalDescription = description
         self.goalType = type
+    }
+    
+    // Save to with Core Data
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            print("data saved successfully")
+            completion(true)
+            
+        } catch {
+            debugPrint("could not save: \(error.localizedDescription)")
+            completion(false)
+        }
+        
     }
     
     // Actions
     @IBAction func createGoalButtonWasPressed(_ sender: Any) {
-        // Pass data into core data goal model
+        if pointsTextField.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+        
     }
     
+    @IBAction func backButtonWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
     
 }
