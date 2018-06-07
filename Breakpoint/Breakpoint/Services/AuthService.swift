3eda5321c7a11ed89f598b1f32887574917dafa2
Andrew - MacMini <andrewmiotke@gmail.com>
2018-06-06 22:09:48 -0700

    Breakpoint is currently broken. I have asked teh question

--- Breakpoint/Breakpoint/Services/AuthService.swift
+++ Breakpoint/Breakpoint/Services/AuthService.swift
@@ -13,12 +13,12 @@ class AuthService {
  
     static let instance = AuthService()
     
-    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
         Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
             guard let user = user else {
                 userCreationComplete(false, error)
                 return
-        }
+            }
             let userData = ["provider": user.providerID, "email": user.email]
             DataService.instance.createDBUser(uid: user.uid, userData: userData)
             userCreationComplete(true, nil)
