d736ee9c5c4a5f30298fae7c1893697ea778439e
andrew miotke <andrewmiotke@gmail.com>
2018-06-13 18:38:00 -0700

    Styled the labels

---
+++ VW Golf/VW Golf/View/LabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  LabelStyle.swift
+//  VW Golf
+//
+//  Created by Andrew Miotke on 6/13/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class LabelStyle: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
