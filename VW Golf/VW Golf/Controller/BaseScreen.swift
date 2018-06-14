78517bf54b649fff663278a789157bf66a2af056
andrew miotke <andrewmiotke@gmail.com>
2018-06-13 21:03:37 -0700

    Added an app icon and changed the display name

--- VW Golf/VW Golf/Controller/BaseScreen.swift
+++ VW Golf/VW Golf/Controller/BaseScreen.swift
@@ -31,7 +31,7 @@ class BaseScreen: UIViewController {
     
     func setInitialView() {
         carModelLabel.text = "Volkswagen GTI"
-        carImage.image = UIImage(named: "mk4_gti")
+        carImage.image = UIImage(named: "gti")
         carDescription.text = "Choose a GTI Generation"
         chooseModelButton.setTitle("Choose Model", for: .normal)
     }
