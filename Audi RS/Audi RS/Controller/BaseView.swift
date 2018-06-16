c158112e1a67bae98c7614499197647bd72deb2d
andrew miotke <andrewmiotke@gmail.com>
2018-06-15 20:01:57 -0700

    Started setting up the RS Selector

--- Audi RS/Audi RS/Controller/BaseView.swift
+++ Audi RS/Audi RS/Controller/BaseView.swift
@@ -20,12 +20,7 @@ class BaseView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    }    
 }
 
 extension SelectRSView: RSSelectionDelegate {
--- Audi RS/Audi RS/Controller/SelectRSView.swift
+++ Audi RS/Audi RS/Controller/SelectRSView.swift
@@ -14,4 +14,27 @@ class SelectRSView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // Actions
+    @IBAction func b4RS2(_ sender: Any) {
+        print("b4 RS2")
+    }
+    
+    @IBAction func b5RS4(_ sender: Any) {
+        print("b5 RS4")
+    }
+    
+    @IBAction func b7RS4(_ sender: Any) {
+        print("b7 RS4")
+    }
+    
+    @IBAction func b8RS4(_ sender: Any) {
+        print("b8 RS4")
+    }
+    
+    @IBAction func b9RS4(_ sender: Any) {
+        print("b9 RS4")
+    }
+    
+    
 }
