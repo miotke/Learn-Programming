25118c18ad89d8104496440421ed5bccb7d1f82b
andrew miotke <andrewmiotke@gmail.com>
2018-06-22 16:24:26 -0700

    Creating one last table view

---
+++ TableViews/TableViews/Controller/GusTableView.swift
@@ -0,0 +1,21 @@
+//
+//  GusTableView.swift
+//  TableViews
+//
+//  Created by Andrew Miotke on 6/22/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class GusTableView: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
