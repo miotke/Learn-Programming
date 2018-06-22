b49a8532516c1b436e49d28ab1977328e7d7a815
andrew miotke <andrewmiotke@gmail.com>
2018-06-22 16:36:38 -0700

    Made another table view

--- TableViews/TableViews/Controller/GusTableView.swift
+++ TableViews/TableViews/Controller/GusTableView.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class GusTableView: UIViewController {
-
+    
+    // Outlets
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
+    var gusIsOurPup = ["Gus", "is", "our", "pup"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.backgroundColor = UIColor.purple
+        label.text = "Gus is our pup!"
+        label.font = UIFont(name: "Avenir Next", size: 30)
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+}
+
+extension GusTableView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gusIsOurPup.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GusCell", for: indexPath)
+        cell.textLabel?.text = gusIsOurPup[indexPath.row]
+        return cell
+    }
+    
+    
 }
