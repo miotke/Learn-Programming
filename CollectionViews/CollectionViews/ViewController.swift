4d55990c7ac7caa7e0528a2bdf87506225886db8
andrew miotke <andrewmiotke@gmail.com>
2018-06-26 07:35:48 -0700

    Attemptin to learn CollectionViews

---
+++ CollectionViews/CollectionViews/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CollectionViews
+//
+//  Created by Andrew Miotke on 6/26/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
