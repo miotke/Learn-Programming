282c82fd1b09d02594fe235e173e19a4cecf09bc
andrew miotke <andrewmiotke@gmail.com>
2018-06-26 17:57:28 -0700

    Setup initial view

---
+++ FlickerSearch/FlickerSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickerSearch
+//
+//  Created by Andrew Miotke on 6/26/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+final class FlickrPhotosViewController: UICollectionViewController {
+
+    // MARK: Properties
+    fileprivate let reuseIdentifier = "FlickrCell"
+    fileprivate let sectionInset = UIEdgeInsetsMake(50.0, 20.0, 50.0, 20.0)
+}
