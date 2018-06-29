f224dfe06928471ef5083dd9b94cb8187f2fa4b5
andrew miotke <andrewmiotke@gmail.com>
2018-06-28 20:05:35 -0700

    Attempting to work with Dog API

---
+++ Dogs/Dogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by Andrew Miotke on 6/28/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
