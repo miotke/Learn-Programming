4e43736f5c97b53f79d92bec4a6a4c5a05432bc9
andrew miotke <andrewmiotke@gmail.com>
2018-06-29 20:26:38 -0700

    Creating a new table view

---
+++ TableViews/TableViews/Controller/GusViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GusViewController.swift
+//  TableViews
+//
+//  Created by Andrew Miotke on 6/29/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class GusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
