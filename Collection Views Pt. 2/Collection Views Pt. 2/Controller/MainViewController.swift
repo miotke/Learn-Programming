eca1d96c3056a5d299031b2b9a6ef889859c49cd
andrew miotke <andrewmiotke@gmail.com>
2018-07-17 18:05:01 -0700

    Constraits on setup

---
+++ Collection Views Pt. 2/Collection Views Pt. 2/Controller/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Collection Views Pt. 2
+//
+//  Created by Andrew Miotke on 7/17/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
