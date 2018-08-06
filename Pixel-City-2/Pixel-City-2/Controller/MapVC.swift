06180175f0ea052d72de0e7d293c989e7a9353da
Andrew Miotke <andrewmiotke@gmail.com>
2018-08-05 18:44:09 -0700

    Learning more about connecting to API

---
+++ Pixel-City-2/Pixel-City-2/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pixel-City-2
+//
+//  Created by Andrew Miotke on 8/5/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
