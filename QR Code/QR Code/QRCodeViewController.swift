99e7e68f579a4d9c0927ce7502c1fed1897144fb
Andrew Miotke <andrewmiotke@gmail.com>
2018-08-20 13:10:16 -0700

    Initial view

---
+++ QR Code/QR Code/QRCodeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  QRCodeViewController.swift
+//  QR Code
+//
+//  Created by Andrew Miotke on 8/20/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var QRCode: UIView!
+    @IBOutlet weak var explainationLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    func setupView() {
+        titleLabel.text = "Scan code at checkout"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: "Avenir Next", size: 20)
+        
+        QRCode.backgroundColor = UIColor.red
+        
+        explainationLabel.text = "The above QR code will allow you to receive various discounts at checkout."
+        explainationLabel.textAlignment = .center
+        explainationLabel.font = UIFont(name: "Avenir Next", size: 20)
+    }
+}
+
