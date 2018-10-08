60039a0bef7e31ac0bec5800514f9fdd3c23f843
Andrew Miotke <andrewmiotke@gmail.com>
2018-10-07 17:51:07 -0700

    Working through the Angela Yu course

---
+++ Angela Yu/Variables, Constants, and Data Types.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// MARK: Variables
+var myAge = 30
+
+// MARK: Constants
+let name = "Andrew"
+let lastName = "Miotke"
+let myAgeInTenYears = myAge + 10
+let myFullName = name + " ...Guess my last name"
+let realFullName = "\(name) \(lastName)"
+
+print("\(name) \(lastName) and I will be \(myAgeInTenYears) in ten years")
+
+// MARK: Data Types
+var variable = "variables can mutate"
+let constant = "constants can't mutate"
+let booleans = true
+let float = 1.3
+let double = 3.141
