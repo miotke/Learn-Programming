f3c4a9389b74a7a6e2c96b609be2478bbd65fd47
Andrew Miotke <andrewmiotke@gmail.com>
2018-10-21 15:48:13 -0700

    Creating a Xylophone

--- Angela Yu/Playgrounds/Loops.playground/Contents.swift
+++ Angela Yu/Playgrounds/Loops.playground/Contents.swift
@@ -1,27 +1,31 @@
 import UIKit
 
-//for numbers in 1...10 {
-//    print(numbers)
-//}
-//
-//for numbered in 1..<10 {
-//    print(numbered)
-//}
-//
-//for number in 1..<10 where number % 2 == 0 {
-//    print(number)
-//}
+for numbers in 1...10 {
+    print(numbers)
+}
+
+for numbered in 1..<10 {
+    print(numbered)
+}
+
+for number in 1..<10 where number % 2 == 0 {
+    print(number)
+}
 
 // 99 bottles of beer
-func beerSong() -> String {
+func beerSong(forThisManyBottlesOfBeer totalNumberOfBottles: Int) -> String {
     var lyrics = ""
+    
+    for number in (1...totalNumberOfBottles).reversed() {
+        let subtractABottle = number - 1
+        let newLine = "\n\(number) bottles of beer on the wall, \(number) bottles of beer. \nTake one down and pass it around \(subtractABottle) bottles of beer on the wall. \n"
+        
+        lyrics += newLine
+    }
+    
+    lyrics += "\nNo more bottles of beer on the wall, no more bottles ofbeer. \nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     
     return lyrics
 }
 
-print(beerSong())
-
-for bottlesOfBeer in 1...99 {
-    let subtractBottle = bottlesOfBeer - 1
-    print ("\(bottlesOfBeer) bottles of beer on the wall, \(bottlesOfBeer) bottles of beer. Take one down and pass it around, \(subtractBottle) of beer on the wall.")
-}
+print(beerSong(forThisManyBottlesOfBeer: 99))
