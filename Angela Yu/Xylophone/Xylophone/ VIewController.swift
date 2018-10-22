a9c0f834aa711d40e4f6b785c81d55ec3bbe9f5d
Andrew <andrewmiotke@gmail.com>
2018-10-21 17:04:58 -0700

    Xylophone complete

--- Angela Yu/Xylophone/Xylophone/ VIewController.swift
+++ Angela Yu/Xylophone/Xylophone/ VIewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.black
+    }
+    
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
     }
 
+    // MARK: Actions
     @IBAction func notePressed(_ sender: UIButton) {
-    
-        
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        playSound()
     }
 }
