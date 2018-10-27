859bc0c574ce158fbd23f463626d66e87812c2ff
Andrew <andrewmiotke@gmail.com>
2018-10-27 10:27:20 -0700

    Created a local variable

--- Angela Yu/Xylophone/Xylophone/ VIewController.swift
+++ Angela Yu/Xylophone/Xylophone/ VIewController.swift
@@ -12,7 +12,6 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
-    var selectedSoundFileName = ""
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     
@@ -21,8 +20,8 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         view.backgroundColor = UIColor.black
     }
     
-    func playSound() {
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
@@ -35,7 +34,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     // MARK: Actions
     @IBAction func notePressed(_ sender: UIButton) {
-        selectedSoundFileName = soundArray[sender.tag - 1]
-        playSound()
+        playSound(soundFileName: soundArray[sender.tag - 1])
     }
 }
