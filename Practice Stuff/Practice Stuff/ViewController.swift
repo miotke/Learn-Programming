b3c15c2b977164f5176b00cbc6e7193d7756f9ac
Andrew Miotke <andrewmiotke@gmail.com>
2018-12-06 10:07:48 -0800

    Creating a quick parctice app

---
+++ Practice Stuff/Practice Stuff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice Stuff
+//
+//  Created by Andrew Miotke on 12/6/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
