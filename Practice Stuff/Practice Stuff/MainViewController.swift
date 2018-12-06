ae3810b4ecc570b6352067e069d561af46cf0fb9
Andrew Miotke <andrewmiotke@gmail.com>
2018-12-06 11:30:31 -0800

    Created buttons and labels programatically and created objects of each buttons and labels

---
+++ Practice Stuff/Practice Stuff/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Practice Stuff
+//
+//  Created by Andrew Miotke on 12/6/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let buttons = Buttons()
+    let labels = Labels()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupButtonViews()
+        setupLabelViews()
+    }
+
+    func setupButtonViews() {
+        self.view.backgroundColor = UIColor.black
+        buttons.makeOvalButton()
+        buttons.makeRectangleButton()
+        view.addSubview(buttons)
+    }
+    
+    func setupLabelViews() {
+        labels.createWelcomeLabel()
+        labels.createGoodbyeLabel()
+        view.addSubview(labels)
+    }
+}
+
