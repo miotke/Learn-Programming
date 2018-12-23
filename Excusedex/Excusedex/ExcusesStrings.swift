30d40bc1d59377e88193ba3a46e3ecd53306c6f0
Andrew Miotke <andrewmiotke@gmail.com>
2018-12-23 15:03:07 -0800

    Was working on another project

---
+++ Excusedex/Excusedex/ExcusesStrings.swift
@@ -0,0 +1,20 @@
+//
+//  ExcusesStrings.swift
+//  Excusedex
+//
+//  Created by Andrew Miotke on 12/22/18.
+//  Copyright © 2018 Andrew Miotke. All rights reserved.
+//
+
+import Foundation
+
+class Exuses {
+    
+    let excuses = [
+            "I can't today.",
+            "My leg hurts.",
+            "I have other plans.",
+            "I have a date",
+            "I'm staying late a work."
+    ]
+}
