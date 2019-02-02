dcaad91ca29952a6c402d27910576b89863eb967
Andrew Miotke <andrewmiotke@gmail.com>
2019-02-02 15:41:13 -0800

    more math in C

--- C/math.c
+++ C/math.c
@@ -3,6 +3,13 @@
 int main(void) {
   int x = 3;
   int y = 3;
+  int a = 1;
+  int b = 10;
+  int m = 100;
+  int n = 1000;
 
   printf("%i + %i = %i\n", x, y, x + y);
+  printf("%i - %i = %i\n", a, b, a - b);
+  printf("%i / %i = %i\n", m, n, m / n);
+  printf("%i * %i = %i\n", m, n, m * n);
 }
