decd22d092b7220734fc2544f6c1f1e39bffbcad
Andrew Miotke <andrewmiotke@gmail.com>
2019-02-19 21:06:39 -0800

    learned how to create a simple class in python

---
+++ Python/class.py
@@ -0,0 +1,8 @@
+class GusClass:
+	dogName = "Gus"
+	dogAge = 2.5
+	dogBreed = "Royal Frenchel"
+
+myDog = GusClass()
+print("My dog's name is " + myDog.dogName + " and is of breed " + myDog.dogBreed + ".")
+print(myDog.dogAge)
