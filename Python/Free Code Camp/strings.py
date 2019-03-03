ff9ab7b9f7e6148f3f58ec1ab64da9d119606d55
Andrew Miotke <andrewmiotke@gmail.com>
2019-03-03 15:41:42 -0800

    more python

---
+++ Python/Free Code Camp/strings.py
@@ -0,0 +1,17 @@
+print("first strings")
+print("Second string with a line break \n")
+print("This string is \"cool\"")
+
+phrase = "Cool string"
+print(phrase)
+
+# Contatination 
+print(phrase + " is a "+ phrase)
+
+# function on strings
+print(phrase.lower())
+print(phrase.split()) # splits a string into a an array
+print(phrase.title())
+print(phrase.upper())
+
+
