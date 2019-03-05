de95cffb78d8ba56e2c2823d1e5b0bffb9e259bc
Andrew Miotke <andrewmiotke@gmail.com>
2019-03-04 20:29:55 -0800

    more python

---
+++ Python/class2.py
@@ -0,0 +1,9 @@
+class ClassTwo:
+    first_name = input("What is your name? ")
+    age = input("What's your age? ")
+
+create_class = ClassTwo() 
+
+print(create_class.first_name)
+print(create_class.age)
+print(create_class.first_name + create_class.age)
