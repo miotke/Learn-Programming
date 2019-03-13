d02245befac6a739166ca30fdeef6f3103e510d6
Andrew Miotke <andrewmiotke@gmail.com>
2019-03-12 19:13:39 -0700

    adding more python

--- Python/guess_my_number.py
+++ Python/guess_my_number.py
@@ -1,13 +1,6 @@
-number_to_guess = 1
+from random import randint
 
-# this shouldbe a for loop, this needs to be a random int
-for number in range(1, 10):
-    number_to_guess = number
-    print(number_to_guess)
+random_number = randint(1, 10)
 
-users_number = input("Guess a number: ")
-
-if number_to_guess == users_number:
-    print(f"You guessed right. Your number: {users_number}, Computer's number: {number_to_guess}")
-else:
-    print(f"Better luck next time. Computer number: {number_to_guess}")
+users_guess = input("Guess a number: ")
+print(random_number + users_guess)
