1e6c37c413c953d01966db1a19cea8e071b91f01
Andrew Miotke <andrewmiotke@gmail.com>
2019-03-16 09:59:28 -0700

    Starting a new project

---
+++ LgText/LgText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LgText
+//
+//  Created by Andrew Miotke on 3/16/19.
+//  Copyright © 2019 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
