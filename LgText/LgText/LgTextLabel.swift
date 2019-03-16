c601b3264b4971c481249e374e8ab39140bfce03
Andrew Miotke <andrewmiotke@gmail.com>
2019-03-16 10:09:19 -0700

    Making the text label

---
+++ LgText/LgText/LgTextLabel.swift
@@ -0,0 +1,17 @@
+//
+//  TextLabel.swift
+//  LgText
+//
+//  Created by Andrew Miotke on 3/16/19.
+//  Copyright © 2019 Andrew Miotke. All rights reserved.
+//
+
+import UIKit
+
+class LgTextLabel {
+    let lgTextLabel = UILabel()
+    
+    func createLgTextLabel() {
+        
+    }
+}
--- LgText/LgText/ViewController.swift
+++ LgText/LgText/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let lgTextLabel = LgTextLabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
