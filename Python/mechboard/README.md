4fdbac90fd2cc1ba1ac401ac829ee16093955a3f
Andrew Miotke <andrewmiotke@gmail.com>
2019-05-12 17:57:53 -0700

    built the initial layout

--- Python/mechboard/README.md
+++ Python/mechboard/README.md
@@ -4,6 +4,8 @@ Website for the iOS app that I'm building
 Following this guide to learn Flask
 https://www.youtube.com/watch?v=MwZwr5Tvyxo
 
+* Currently on video 3
+
 ### Start Virtual Env
 `source ~/python_environments/mechboard_env/bin/activate`
 
--- Python/mechboard/flaskblog.py
+++ Python/mechboard/flaskblog.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Gus Gus",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "May 12, 2019"
+    },
+    {
+        "author": "Polly Polly",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "May 12, 2019"
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Hello world!</h1>"
+@app.route("/home")
+def home():
+    return render_template("index.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
 
 
 if __name__ == "__main__":
