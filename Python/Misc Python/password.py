6ed621afc3c27cc023520846cb358512e953c848
Andrew Miotke <andrewmiotke@gmail.com>
2019-05-20 21:10:10 -0700

    updated the password generator

--- Python/Misc Python/password.py
+++ Python/Misc Python/password.py
@@ -1,16 +1,26 @@
-# Obviously storing passwords in a text file is a bad idea. Don't do it!
-# This was more so a proof of concept to generate a random string(or in this case a password)
+"""
+Obviously storing passwords in a text file is a bad idea. Don't do it!
+This was more so a proof of concept to generate a random string(or in this case a password)
+"""
 
 import string
 import random
 
-# Function that will generate a random string with a charactor size minimum of 8
+
 def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
- return ''.join(random.choice(chars) for _ in range(size))
+    """ Function that will generate a random string with a charactor size minimum of 8 """
+    return ''.join(random.choice(chars) for _ in range(size))
 
-# Creates a file named 'passwords.txt' and saves the generated string to that file
-# Adds two new lines in between each entry
-passwordFile = open('passwords.txt', 'a')
-passwordFile.write('\n' + '\n' + pw_gen(int(input('How many charactors in your password?'))))
-passwordFile.close()
 
+def write_password():
+    """
+    Creates a file named 'passwords.txt' and saves the generated string to that file
+    Adds two new lines in between each entry
+    """
+    with open('passwords.txt', 'a') as password_file:
+        password_file.write('\n' + '\n' + pw_gen(int(input('How many characters in your password? '))))
+        password_file.close()
+
+
+if __name__ == '__main__':
+    write_password()
