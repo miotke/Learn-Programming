6d7d635afbdddd3018d6c7d5a939029bb8ec93f7
Andrew Miotke <andrewmiotke@gmail.com>
2019-05-27 19:39:14 -0700

    video 4 complete

--- Python/OOP/oop_3.py
+++ Python/OOP/oop_3.py
@@ -2,10 +2,12 @@
 # Using Corey Schafer's YouTube video course
 # Video 3
 
+import datetime
 
 class Employee:
 
-    # class variable
+    # class variables
+    num_of_employees = 0
     raise_amount = 1.04
 
 
@@ -14,6 +16,9 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
+
+        # No need to change the number of employees per class instance
+        Employee.num_of_employees += 1
 
 
     def full_name(self):
@@ -24,13 +29,32 @@ class Employee:
         self.pay = int(self.pay * self.raise_amount)
 
 
+    # overrides the class variable raise_amount to the number that is passed in for amount
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+
+    # Alternative constructor
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+
+    # static methods don't operate on the instance or the class
+    @staticmethod
+    def is_work_day(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+
 emp_1 = Employee('Andrew', 'Miotke', 5000)
-emp_2 = Employee('Gus', 'Maggie', 70000)
+emp_str_1 = "Andrew-Miotke-5000"
+emp_2 = Employee('Gus', 'Gus', 70000)
+emp_3 = Employee("Maggie", "Saunders", 8000)
 
-print(emp_1.email.lower())
-print(emp_2.email)
-print(emp_1.full_name())
-
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+my_date = datetime.date(2019, 5, 27)
+print(Employee.is_work_day(my_date))
---
+++ Python/OOP/oop_4.py
@@ -0,0 +1,72 @@
+# Object Oritented Programming with Python
+# Using Corey Schafer's YouTube video course
+# Video 4
+
+class Employee:
+
+    # class variables
+    num_of_employees = 0
+    raise_amount = 1.04
+
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        # No need to change the number of employees per class instance
+        Employee.num_of_employees += 1
+
+
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+class Developer(Employee):
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, prog_language):
+        super().__init__(first, last, pay)
+        self.prog_language = prog_language
+
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+
+    def add_employee(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+
+    def remove_employee(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+
+    def print_employees(self):
+        for emp in self.employees:
+            print(f"---> {emp.full_name()}")
+
+
+dev_1 = Developer('Andrew', 'Miotke', 50000, "python")
+dev_2 = Developer('Gus', 'Gus', 70000, "swift")
+
+mgr_1 = Manager("Sue", "Smith", 9000, [dev_1])
+
+
+print(isinstance(mgr_1, Manager))
+print(isinstance(mgr_1, Employee))
+
+print(issubclass(Developer, Employee))
