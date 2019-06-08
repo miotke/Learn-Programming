b90ac944a490610f1c98be97741ecd19a0cf515f
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-08 16:25:11 -0700

    uploads profile picture and resizes it

--- Python/flask_blog/flask_blog/forms.py
+++ Python/flask_blog/flask_blog/forms.py
@@ -5,6 +5,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flask_blog.models import User
 
+
 class RegisterationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators = [DataRequired(), Email(), ])
@@ -35,7 +36,7 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators = [DataRequired(), Email(), ])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
     submit = SubmitField('Update')
 
 
