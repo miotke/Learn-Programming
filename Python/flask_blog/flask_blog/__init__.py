9b95d051550a6deb9793572f39edcf6823755539
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-08 15:17:19 -0700

    more routes for login

--- Python/flask_blog/flask_blog/__init__.py
+++ Python/flask_blog/flask_blog/__init__.py
@@ -3,12 +3,16 @@ from flask_sqlalchemy import  SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ce9673c867e5b6fdf0eed4472de6ca0f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+# login_manager.login.message_category = 'info'
+
 
 from flask_blog import routes
 
