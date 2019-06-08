96a9ed643827f31917b24663891063703d1a11c4
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-07 17:36:01 -0700

    starting chapter 1

---
+++ Python/Automate_the_Boring_Stuff/Chapter_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Expresions
+print(2 + 2)
+print(2 + 3 * 6)
+print((2 + 3) * 6)
+print(48565878 * 578453)
+print(2 ** 8)
+print(23 / 7)
+print(23 % 7)
+print(2       +            2)
+print((5 - 1) * ((7 + 1) / (3 -1)))
+
+
+# Data types
+a_integer = 31
+a_float = 32.2
+a_string = "This is a string"
+
+
+alice = "Alice"
+bob = "Bob"
+print(alice + bob)
