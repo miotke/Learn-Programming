ac987f7a12ab2a44dd69d59f2d0a0a42c005efb8
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-09 17:37:05 -0700

    Setting up oath

--- Python/Projects/MS_Calendar/graph_tutorial/tutorial/views.py
+++ Python/Projects/MS_Calendar/graph_tutorial/tutorial/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def home(request):
+    context = initialize_context(request)
+    return render(request, "tutorial/home.html", context)
+
+
+def initialize_context(request):
+    context = {}
+
+    # Check for any errors in the session
+    error = request.session.pop("flash_error", None)
+
+    if error != None:
+        context["errors"] = []
+        context["errors"].append(error)
+
+    # Check for user in the session
+    context["user"] = request.session.get("user", {"is_authenticated": False})
+    return context
