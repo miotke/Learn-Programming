a272138648308693f8beaff3cc5877f16896aa72
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-09 18:14:37 -0700

    taking a break

--- Python/Projects/MS_Calendar/graph_tutorial/tutorial/views.py
+++ Python/Projects/MS_Calendar/graph_tutorial/tutorial/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from tutorial.auth_helper import get_sign_in_url, get_token_from_code
+from tutorial.graph_helper import get_user
 
 # Create your views here.
 def home(request):
@@ -38,6 +39,9 @@ def callback(request):
     expected_state = request.session.pop("auth_state", "")
     # Make the token request
     token = get_token_from_code(request.get_full_path(), expected_state)
+
+    # Get the user's profile
+    user = get_user(token)
     # Temporary! Save the reponse in the error so it's displayed
-    request.session["flash_error"] = {"message": "Token retrieved", "debug": format(token)}
+    request.session["flash_error"] = {'message': 'Token retrieved', 'debug': 'User: {0}\nToken: {1}'.format(user, token)}
     return HttpResponseRedirect(reverse("home"))
