e5f0460b191044825ad4254d3c83ac88c4ada69d
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-16 16:48:20 -0700

    video 3 complete

--- Python/Projects/django_blog/django_project/blog/views.py
+++ Python/Projects/django_blog/django_project/blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Logic for site routes
 
+# Dummy data
+posts = [
+    {
+        'author': 'Andrew M',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 16, 2019'
+    },
+    {
+        'author': 'Gus',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 2, 2019'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, "blog/about.html", {'title': 'About'})
