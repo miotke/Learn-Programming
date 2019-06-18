9eef992c7e3272f0481af08abdca95ad74623583
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-17 21:08:12 -0700

    video 6 complete and Django admin is super dope

--- Python/Projects/django_blog/README.md
+++ Python/Projects/django_blog/README.md
@@ -1,10 +1,10 @@
 # Flask Blog
 
 Following this guide to learn Django:
-https://www.youtube.com/watch?v=a48xeeo5Vnk&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=2
 
+https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6
 
-* Currently on video 4
+* Currently on video 6
 
 ### Start Virtual Env
 `source ~/python_environments/django_blog/bin/activate`
--- Python/Projects/django_blog/django_project/blog/views.py
+++ Python/Projects/django_blog/django_project/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Logic for site routes
 
-# Dummy data
-posts = [
-    {
-        'author': 'Andrew M',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 16, 2019'
-    },
-    {
-        'author': 'Gus',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 2, 2019'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
