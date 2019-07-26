f25ec074c41d90e01254d915ab7ccdfe76f2dd73
Andrew Miotke <andrewmiotke@gmail.com>
2019-07-25 18:08:20 -0700

    fixed a couple errors and migrated

--- Python/Projects/MDN_Local_Library/locallibrary/catalog/models.py
+++ Python/Projects/MDN_Local_Library/locallibrary/catalog/models.py
@@ -83,19 +83,19 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = model.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
 
-        class Meta:
-            ordering = ['last_name', 'first_name']
-
-
-            def get_absolute_url(self):
-                """ Returns the url to access a particular author instance """
-
-                return reverse('author-detail', args=[str(self.id)])
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
 
-            def __str__(self):
-                """ String for representing the model object """
+    def get_absolute_url(self):
+        """ Returns the url to access a particular author instance """
 
-                return f'{self.last_name}, {self.first_name}'
+        return reverse('author-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+        """ String for representing the model object """
+
+        return f'{self.last_name}, {self.first_name}'
