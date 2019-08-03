e9bed0f1292abb59047d7856d2ddc5651053ad70
Andrew Miotke <andrewmiotke@gmail.com>
2019-08-02 17:54:13 -0700

    Wrote some basic tests for calc.py

--- Python/Testing/basic_testing/test_calc.py
+++ Python/Testing/basic_testing/test_calc.py
@@ -6,5 +6,25 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 10), 20)
+        self.assertEqual(calc.add(-10, -10), -20)
+        self.assertEqual(calc.add(-10, 2), -8)
+
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10, 1), 9)
+        self.assertEqual(calc.subtract(-1, 0), -1)
+        self.assertEqual(calc.subtract(-5, -100), 95)
+
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10, 10), 100)
+        self.assertEqual(calc.multiply(-10, 3), -30)
+        self.assertEqual(calc.multiply(-10, -10), 100)
+
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-10, -10), 1)
+        self.assertEqual(calc.divide(0, 10), 0)
+
