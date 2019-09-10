f24e7c0f0c0cffe3d2c1b986c11ee5500f18f1b0
Andrew Miotke <andrewmiotke@gmail.com>
2019-09-09 19:14:16 -0700

    Working on the index view

--- Python/Projects/onboarding/onboarding/index/views.py
+++ Python/Projects/onboarding/onboarding/index/views.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from google.models import Google
 
-
-def index(request):
-    return HttpResponse("Hello")
+class GoogleOnboarding(CreateView, forms.Form):
+    model = Google
