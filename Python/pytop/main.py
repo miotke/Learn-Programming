3b374c26c3379950f101c92f1d6d4104499683b1
Andrew Miotke <andrewmiotke@gmail.com>
2019-10-13 20:59:03 -0700

    Playing around a bit more

--- Python/pytop/main.py
+++ Python/pytop/main.py
@@ -4,12 +4,13 @@ import os
 
 def get_cpu():
     cpu_precent = psutil.cpu_percent()
-    print(cpu_precent)
+    print(f'CPU Precent: {cpu_precent}')
 
 
 def get_virtual_memory():
     virtual_memory = psutil.virtual_memory()
-    print(virtual_memory)
+    print(f'Virtual Memory:{virtual_memory}')
+
 
 def main():
     get_cpu()
