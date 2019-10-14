fbd4995ee7cc62b820637d88db04bce48ac4ee7e
Andrew Miotke <andrewmiotke@gmail.com>
2019-10-13 20:31:38 -0700

    Playing with psutils

--- Python/pytop/main.py
+++ Python/pytop/main.py
@@ -1,3 +1,21 @@
 import psutil
+import os
 
+
+def get_cpu():
+    cpu_precent = psutil.cpu_percent()
+    print(cpu_precent)
+
+
+def get_virtual_memory():
+    virtual_memory = psutil.virtual_memory()
+    print(virtual_memory)
+
+def main():
+    get_cpu()
+    get_virtual_memory()
+
+
+if __name__ == '__main__':
+    main()
 
