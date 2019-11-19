dbf36676b84c901c1e032856de2b36e22591bc67
Andrew Miotke <andrewmiotke@gmail.com>
2019-11-18 20:32:36 -0800

    Created a UIAlert to create to todo items - Created a UIAlert with an action and text field that adds a new item to the array of hardcoded items. - Reload data in the TableView

--- Swift/Projects/Angela Yu/Todoey/Todoey/TodoListViewController.swift
+++ Swift/Projects/Angela Yu/Todoey/Todoey/TodoListViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demigorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destory Demigorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     
@@ -39,5 +38,25 @@ class TodoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    // MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
