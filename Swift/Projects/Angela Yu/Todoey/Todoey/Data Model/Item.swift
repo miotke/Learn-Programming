b6eb5f2b1b4542ec6105a1a213f8fb0c4ae93410
Andrew Miotke <andrewmiotke@gmail.com>
2019-11-19 21:57:32 -0800

    Started created a data model

---
+++ Swift/Projects/Angela Yu/Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andrew Miotke on 11/19/19.
+//  Copyright © 2019 FallenLeaf. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    init() {
+        let title: String
+        let Done: Bool
+    }
+}
