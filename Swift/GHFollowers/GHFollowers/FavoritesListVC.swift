6837bcabb7c16fbe4ed6147750ded8cb66145ace
Andrew Miotke <andrewmiotke@gmail.com>
2020-01-14 11:28:40 -0800

    Initial commit of new project

---
+++ Swift/GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 1/14/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemPink
+    }
+}
