673c9e266b3f9a0ee33be8344ffb4e2d24a323f6
Andrew Miotke <andrewmiotke@gmail.com>
2020-01-18 10:18:00 -0800

    Created some new custom elements

---
+++ Swift/GHFollowers/GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 1/18/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Swift/GHFollowers/GHFollowers/GFTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 1/18/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
