cf8f5cb3d0e217fa22e44b7047b6be7a694472c0
Andrew Miotke <andrewmiotke@gmail.com>
2020-01-18 11:24:57 -0800

    Custom Alert view now appears, hs a bug where the message doesnt appear

--- Swift/GHFollowers/GHFollowers/SearchVC.swift
+++ Swift/GHFollowers/GHFollowers/SearchVC.swift
@@ -39,7 +39,7 @@ class SearchVC: UIViewController {
     
     @objc func pushFollowerListVC() {
         guard isUserNameEntered else {
-            print("no username")
+            presentGFAlertOnMainThread(title: "Empty username", message: "Please enter a username. We need to know who to look for", buttonTitle: "Okay")
             return
         }
         
---
+++ Swift/GHFollowers/GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 1/18/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
