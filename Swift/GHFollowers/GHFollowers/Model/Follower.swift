83051a21e9e843498b0845224f47e369a29f2797
Andrew Miotke <andrewmiotke@gmail.com>
2020-01-28 21:00:31 -0800

    Created the user and follower models

---
+++ Swift/GHFollowers/GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 1/28/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import Foundation
+
+struct Follow: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ Swift/GHFollowers/GHFollowers/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 1/28/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
