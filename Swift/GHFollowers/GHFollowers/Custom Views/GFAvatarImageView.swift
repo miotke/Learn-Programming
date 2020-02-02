a35e916eb9a1a12c1ec1f6d543ea0cc3f22fb776
Andrew Miotke <andrewmiotke@gmail.com>
2020-02-02 09:49:34 -0800

    Created custom cell

---
+++ Swift/GHFollowers/GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Andrew Miotke on 2/2/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
